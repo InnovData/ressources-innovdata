131535adadbf750824ab51876814a0dac4dec83f
Vincent <50195411+vincentm498@users.noreply.github.com>
2023-05-30 14:34:57 +0200

    update

--- plugins/forms/dist/numbers.dev.js
+++ plugins/forms/dist/numbers.dev.js
@@ -1,84 +1,86 @@
 "use strict";
 
-function quantityInput(element, options) {
-  var spinner = element;
-  var defaultOptions = {
-    min: 1,
-    max: 250,
-    value: 1
-  };
-  options = Object.assign({}, defaultOptions, options);
-  var obj = {
-    input: spinner.querySelector('input[type="number"]'),
-    init: function init() {
-      this.setup();
-      this.events();
-      return this;
-    },
-    setup: function setup() {
-      this.input.value = options.value;
-      this.max = options.max;
-      this.min = options.min;
-      var qNav = document.createElement("div");
-      var qUp = document.createElement("div");
-      var qDown = document.createElement("div");
-      qNav.setAttribute("class", "quantity__nav");
-      qUp.setAttribute("class", "quantity__button--up");
-      qDown.setAttribute("class", "quantity__button--down"); // qUp.innerHTML = "+";
-      // qDown.innerHTML = "-";
+window.addEventListener("DOMContentLoaded", function (event) {
+  function quantityInput(element, options) {
+    var spinner = element;
+    var defaultOptions = {
+      min: 1,
+      max: 250,
+      value: 1
+    };
+    options = Object.assign({}, defaultOptions, options);
+    var obj = {
+      input: spinner.querySelector('input[type="number"]'),
+      init: function init() {
+        this.setup();
+        this.events();
+        return this;
+      },
+      setup: function setup() {
+        this.input.value = options.value;
+        this.max = options.max;
+        this.min = options.min;
+        var qNav = document.createElement("div");
+        var qUp = document.createElement("div");
+        var qDown = document.createElement("div");
+        qNav.setAttribute("class", "quantity__nav");
+        qUp.setAttribute("class", "quantity__button--up");
+        qDown.setAttribute("class", "quantity__button--down"); // qUp.innerHTML = "+";
+        // qDown.innerHTML = "-";
 
-      qNav.appendChild(qUp);
-      qNav.appendChild(qDown);
-      spinner.appendChild(qNav);
-      this.btnUp = spinner.querySelector(".quantity__button--up");
-      this.btnDown = spinner.querySelector(".quantity__button--down");
-    },
-    trigger: function trigger() {
-      var event = document.createEvent("HTMLEvents");
-      event.initEvent("change", true, false);
-      return event;
-    },
-    events: function events() {
-      var _this = this;
+        qNav.appendChild(qUp);
+        qNav.appendChild(qDown);
+        spinner.appendChild(qNav);
+        this.btnUp = spinner.querySelector(".quantity__button--up");
+        this.btnDown = spinner.querySelector(".quantity__button--down");
+      },
+      trigger: function trigger() {
+        var event = document.createEvent("HTMLEvents");
+        event.initEvent("change", true, false);
+        return event;
+      },
+      events: function events() {
+        var _this = this;
 
-      this.btnUp.addEventListener("click", function () {
-        var oldValue = parseFloat(_this.input.value);
-        var newVal;
+        this.btnUp.addEventListener("click", function () {
+          var oldValue = parseFloat(_this.input.value);
+          var newVal;
 
-        if (oldValue >= _this.max) {
-          newVal = oldValue;
-        } else {
-          newVal = oldValue + 1;
-        }
+          if (oldValue >= _this.max) {
+            newVal = oldValue;
+          } else {
+            newVal = oldValue + 1;
+          }
 
-        _this.input.value = newVal;
+          _this.input.value = newVal;
 
-        _this.input.dispatchEvent(_this.trigger());
-      });
-      this.btnDown.addEventListener("click", function () {
-        var oldValue = parseFloat(_this.input.value);
-        var newVal;
+          _this.input.dispatchEvent(_this.trigger());
+        });
+        this.btnDown.addEventListener("click", function () {
+          var oldValue = parseFloat(_this.input.value);
+          var newVal;
 
-        if (oldValue <= _this.min) {
-          newVal = oldValue;
-        } else {
-          newVal = oldValue - 1;
-        }
+          if (oldValue <= _this.min) {
+            newVal = oldValue;
+          } else {
+            newVal = oldValue - 1;
+          }
 
-        _this.input.value = newVal;
+          _this.input.value = newVal;
 
-        _this.input.dispatchEvent(_this.trigger());
-      });
-      this.input.addEventListener("change", function () {
-        if (parseInt(_this.input.value, 16) < _this.min) {
-          _this.input.value = _this.min;
-        }
+          _this.input.dispatchEvent(_this.trigger());
+        });
+        this.input.addEventListener("change", function () {
+          if (parseInt(_this.input.value, 16) < _this.min) {
+            _this.input.value = _this.min;
+          }
 
-        if (parseInt(_this.input.value, 16) > _this.max) {
-          _this.input.value = _this.max;
-        }
-      });
-    }
-  };
-  return obj.init();
-}
+          if (parseInt(_this.input.value, 16) > _this.max) {
+            _this.input.value = _this.max;
+          }
+        });
+      }
+    };
+    return obj.init();
+  }
+});
--- plugins/forms/numbers.js
+++ plugins/forms/numbers.js
@@ -1,81 +1,83 @@
-function quantityInput(element, options) {
-    const spinner = element;
+window.addEventListener("DOMContentLoaded", (event) => {
+    function quantityInput(element, options) {
+        const spinner = element;
 
-    const defaultOptions = {
-        min: 1,
-        max: 250,
-        value: 1,
-    };
+        const defaultOptions = {
+            min: 1,
+            max: 250,
+            value: 1,
+        };
 
-    options = Object.assign({}, defaultOptions, options);
+        options = Object.assign({}, defaultOptions, options);
 
-    const obj = {
-        input: spinner.querySelector('input[type="number"]'),
-        init() {
-        this.setup();
-        this.events();
-        return this;
-        },
-        setup() {
-        this.input.value = options.value;
-        this.max = options.max;
-        this.min = options.min;
+        const obj = {
+            input: spinner.querySelector('input[type="number"]'),
+            init() {
+            this.setup();
+            this.events();
+            return this;
+            },
+            setup() {
+            this.input.value = options.value;
+            this.max = options.max;
+            this.min = options.min;
 
-        const qNav = document.createElement("div");
-        const qUp = document.createElement("div");
-        const qDown = document.createElement("div");
+            const qNav = document.createElement("div");
+            const qUp = document.createElement("div");
+            const qDown = document.createElement("div");
 
-        qNav.setAttribute("class", "quantity__nav");
-        qUp.setAttribute("class", "quantity__button--up");
-        qDown.setAttribute("class", "quantity__button--down");
+            qNav.setAttribute("class", "quantity__nav");
+            qUp.setAttribute("class", "quantity__button--up");
+            qDown.setAttribute("class", "quantity__button--down");
 
-        // qUp.innerHTML = "+";
-        // qDown.innerHTML = "-";
-        qNav.appendChild(qUp);
-        qNav.appendChild(qDown);
-        spinner.appendChild(qNav);
+            // qUp.innerHTML = "+";
+            // qDown.innerHTML = "-";
+            qNav.appendChild(qUp);
+            qNav.appendChild(qDown);
+            spinner.appendChild(qNav);
 
-        this.btnUp = spinner.querySelector(".quantity__button--up");
-        this.btnDown = spinner.querySelector(".quantity__button--down");
-        },
-        trigger() {
-        const event = document.createEvent("HTMLEvents");
-        event.initEvent("change", true, false);
-        return event;
-        },
-        events() {
-        this.btnUp.addEventListener("click", () => {
-            const oldValue = parseFloat(this.input.value);
-            let newVal;
-            if (oldValue >= this.max) {
-            newVal = oldValue;
-            } else {
-            newVal = oldValue + 1;
-            }
-            this.input.value = newVal;
-            this.input.dispatchEvent(this.trigger());
-        });
+            this.btnUp = spinner.querySelector(".quantity__button--up");
+            this.btnDown = spinner.querySelector(".quantity__button--down");
+            },
+            trigger() {
+            const event = document.createEvent("HTMLEvents");
+            event.initEvent("change", true, false);
+            return event;
+            },
+            events() {
+            this.btnUp.addEventListener("click", () => {
+                const oldValue = parseFloat(this.input.value);
+                let newVal;
+                if (oldValue >= this.max) {
+                newVal = oldValue;
+                } else {
+                newVal = oldValue + 1;
+                }
+                this.input.value = newVal;
+                this.input.dispatchEvent(this.trigger());
+            });
 
-        this.btnDown.addEventListener("click", () => {
-            const oldValue = parseFloat(this.input.value);
-            let newVal;
-            if (oldValue <= this.min) {
-            newVal = oldValue;
-            } else {
-            newVal = oldValue - 1;
-            }
-            this.input.value = newVal;
-            this.input.dispatchEvent(this.trigger());
-        });
-        this.input.addEventListener("change", () => {
-            if (parseInt(this.input.value, 16) < this.min) {
-            this.input.value = this.min;
-            }
-            if (parseInt(this.input.value, 16) > this.max) {
-            this.input.value = this.max;
-            }
-        });
-        },
-    };
-    return obj.init();
-}
+            this.btnDown.addEventListener("click", () => {
+                const oldValue = parseFloat(this.input.value);
+                let newVal;
+                if (oldValue <= this.min) {
+                newVal = oldValue;
+                } else {
+                newVal = oldValue - 1;
+                }
+                this.input.value = newVal;
+                this.input.dispatchEvent(this.trigger());
+            });
+            this.input.addEventListener("change", () => {
+                if (parseInt(this.input.value, 16) < this.min) {
+                this.input.value = this.min;
+                }
+                if (parseInt(this.input.value, 16) > this.max) {
+                this.input.value = this.max;
+                }
+            });
+            },
+        };
+        return obj.init();
+    }
+});
